06bde0c700149d6340055dae3907fc6a19bccb61
Dinshaw Gobhai <gobhai@gmail.com>
2013-12-11 18:35:24 -0500

    Trying to get homebrew right

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
-class osx_dev_station {
-  class hightower::packages {
-    pkglist = ['postgresql', 'nginx']
-    package { $pkglist:
-      ensure   => installed,
-      provider => brew,
-    }
-  }
-  rbenv::install { 'someuser': }
-  rbenv::compile { '2.0.0-p247': }
-  dotfiles { "dinshaw": gituser: "dinshaw"; }
+class { 'homebrew':
+  user => 'dgobhai',
+}
+package { 'postgresql':
+  ensure   => installed,
+  provider => brew,
+}
+rbenv::install { 'someuser': }
+rbenv::compile { '2.0.0-p247': }
+dotfiles { 'dinshaw':
+  gituser => 'dinshaw',
 }
