8742a1e8b7ed277bb11249110d3808344da69fb6
Dinshaw Gobhai <gobhai@gmail.com>
2013-12-11 12:30:39 -0500

    Use homebrew mod

--- Puppetfile
+++ Puppetfile
@@ -1,6 +1,3 @@
 forge "http://forge.puppetlabs.com"
-mod "rbenv", git: "git@github.com:alup/puppet-rbenv.git"
-mod "redis", git: "git@github.com:thomasvandoren/puppet-redis.git"
-mod "postgres", git: "git@github.com:puppetlabs/puppetlabs-postgresql.git"
 mod "dotfiles", git: "git@github.com:boinger/puppet-dotfiles.git"
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,15 @@
-class jenkins_box {
+class osx_dev_station {
+  class hightower::packages {
+  pkglist = ['postgresql', 'nginx']
+
+  package { $pkglist:
+    ensure   => installed,
+    provider => brew,
+  }
+
   rbenv::install { 'someuser': }
   rbenv::compile { '2.0.0-p247': }
   dotfiles { "dinshaw":
     gituser: "dinshaw";
   }
-  include redis
 }
