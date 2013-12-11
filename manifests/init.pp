9b1185406870955266930d701494b3fa1952b712
Dinshaw Gobhai <gobhai@gmail.com>
2013-12-11 15:17:56 -0500

    fix formating

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,12 @@
 class osx_dev_station {
   class hightower::packages {
-  pkglist = ['postgresql', 'nginx']
-
-  package { $pkglist:
-    ensure   => installed,
-    provider => brew,
+    pkglist = ['postgresql', 'nginx']
+    package { $pkglist:
+      ensure   => installed,
+      provider => brew,
+    }
   }
-
   rbenv::install { 'someuser': }
   rbenv::compile { '2.0.0-p247': }
-  dotfiles { "dinshaw":
-    gituser: "dinshaw";
-  }
+  dotfiles { "dinshaw": gituser: "dinshaw"; }
 }
