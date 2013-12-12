eb003752b7bd30d2af98772237970fcd86765539
Dinshaw Gobhai <gobhai@gmail.com>
2013-12-12 10:32:34 -0500

    dotfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,16 @@ package { 'tree':
 # rbenv::install { 'dgobhai': }
 # rbenv::compile { '2.0.0-p247': }
 
-dotfiles { 'dinshaw':
-  gituser => 'dinshaw',
-}
+dotfiles {'dgobhai':
+      gituser      => 'boinger',
+      giturl       => "git://github.com",
+      project      => "dotfiles",
+      dotfiles_dir => "home",
+      branch       => "master",
+      homedir      => "/home/${name}"
+      clobber      => true,
+      bak_ext      => '.bak',
+      single_pull  => false,
+      rebase       => true,
+      frequency    => 30;
+  }
