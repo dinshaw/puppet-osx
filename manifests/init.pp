9f98588cb130f18b0c1f08a67b0e93c5b59b052b
Dinshaw Gobhai <gobhai@gmail.com>
2013-12-12 10:33:51 -0500

    quotes

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,15 +11,15 @@ package { 'tree':
 # rbenv::compile { '2.0.0-p247': }
 
 dotfiles {'dgobhai':
-      gituser      => 'boinger',
-      giturl       => "git://github.com",
-      project      => "dotfiles",
-      dotfiles_dir => "home",
-      branch       => "master",
-      homedir      => "/home/${name}"
-      clobber      => true,
-      bak_ext      => '.bak',
-      single_pull  => false,
-      rebase       => true,
-      frequency    => 30;
-  }
+    gituser      => 'boinger',
+    giturl       => 'git://github.com',
+    project      => 'dotfiles',
+    dotfiles_dir => 'home',
+    branch       => 'master',
+    homedir      => "/home/${name}"
+    clobber      => true,
+    bak_ext      => '.bak',
+    single_pull  => false,
+    rebase       => true,
+    frequency    => 30;
+}
