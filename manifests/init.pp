59b20ed91628b927228c50a083a81fc5843c511e
Dinshaw Gobhai <gobhai@gmail.com>
2014-01-03 14:03:59 -0500

    dotfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,10 @@ package { 'gtypist':
   require  => Class['homebrew']
 }
 
-# rbenv::install { 'dgobhai': }
-# rbenv::compile { '2.0.0-p247': }
+rbenv::install { 'dgobhai': }
+rbenv::compile { '2.0.0-p247': }
 
 exec { 'dotfiles':
   creates => '/Users/dgobhai/.dotfiles',
-  command => '/usr/bin/git clone https://github.com/dinshaw/dotfiles.git /Users/dgobhai/.dotfiles && cd /Users/dgobhai/.dotfiles && ./rake',
+  command => '/usr/bin/git clone https://github.com/dinshaw/dotfiles.git /Users/dgobhai/.dotfiles && cd /Users/dgobhai/.dotfiles && ./rake install',
 }
