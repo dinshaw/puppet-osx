f8fe629ea204e39130528df8b779e7b7247fc76e
Dinshaw Gobhai <gobhai@gmail.com>
2014-01-03 13:59:35 -0500

    dotfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,6 @@ package { 'gtypist':
 # rbenv::compile { '2.0.0-p247': }
 
 exec { 'dotfiles':
-  creates => '/home/dgobhai/.dotfiles',
+  creates => '/Users/dgobhai/.dotfiles',
   command => '/usr/bin/git clone https://github.com/dinshaw/dotfiles.git /home/dgobhai/.dotfiles && cd /home/dgobhai/.dotfiles && ./rake',
 }
