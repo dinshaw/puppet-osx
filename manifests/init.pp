9f51aaf2f6993101f456a1f8b58a1ca97c746e51
Dinshaw Gobhai <gobhai@gmail.com>
2014-01-10 12:30:25 -0500

    sublime

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,6 +26,13 @@ package { 'SublimeText3':
   provider => 'appdmg',
   source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg';
 }
+file { '/usr/local/bin/subl':
+    ensure  => link,
+    target  => '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl',
+    mode    => '0755',
+    require => Package['SublimeText3'],
+  }
+}
 
 
 exec { 'dotfiles':
